7b3061c126bd92e9813da21f5a4cad9eb840a4fe
bohdans <0000xffff@gmail.com>
2020-01-12 15:33:19 +0200

    initial commit

---
+++ LivePhotos/LPBaseViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LPBaseViewController.swift
+//  LivePhotos
+//
+//  Created by Office Mac on 1/12/20.
+//  Copyright © 2020 Vinso. All rights reserved.
+//
+
+import UIKit
+
+class LPBaseViewController: UIViewController {
+    
+}
---
+++ LivePhotos/LPHomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LivePhotos
+//
+//  Created by Office Mac on 1/12/20.
+//  Copyright © 2020 Vinso. All rights reserved.
+//
+
+import UIKit
+
+final class LPHomeViewController: LPBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
